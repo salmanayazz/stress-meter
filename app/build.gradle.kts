e8c2d8289de0f4a4445b2bc33bdd7c2bfc2a95cd
Salman Ayaz <salmanayazz@gmail.com>
2023-10-19 12:24:24 -0700

    added buttons with onClicks that close activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.flexbox:flexbox:3.0.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.2")
--- app/src/main/java/com/example/stressmeter/ui/ImageConfirmationActivity.kt
+++ app/src/main/java/com/example/stressmeter/ui/ImageConfirmationActivity.kt
@@ -2,6 +2,7 @@ package com.example.stressmeter.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import androidx.lifecycle.ViewModelProvider
 import com.example.stressmeter.R
@@ -18,10 +19,17 @@ class ImageConfirmationActivity : AppCompatActivity() {
         val imageId = intent.getIntExtra(IMAGE_ID_KEY, -1)
 
         if (imageId != -1) {
-            println("in act: $imageId")
             findViewById<ImageView>(R.id.confirm_image_view).setImageResource(
                 imageId
             )
+        }
+
+        findViewById<Button>(R.id.cancel_image).setOnClickListener() {
+            finish()
+        }
+
+        findViewById<Button>(R.id.submit_image).setOnClickListener() {
+            finish()
         }
     }
 }
