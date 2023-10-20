fd5b45c879b456b4c926de7b939d7d1dabc749d2
Salman Ayaz <salmanayazz@gmail.com>
2023-10-19 17:21:33 -0700

    added line chart

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
@@ -50,6 +49,7 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.2")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.2")
     implementation("androidx.gridlayout:gridlayout:1.0.0")
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/stressmeter/ui/stressrecord/StressRecordFragment.kt
+++ app/src/main/java/com/example/stressmeter/ui/stressrecord/StressRecordFragment.kt
@@ -4,10 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.stressmeter.R
 import com.example.stressmeter.databinding.FragmentStressRecordBinding
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
+
 
 class StressRecordFragment : Fragment() {
 
@@ -23,15 +29,22 @@ class StressRecordFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val stressRecordViewModel =
-            ViewModelProvider(this).get(StressRecordViewModel::class.java)
+            ViewModelProvider(this)[StressRecordViewModel::class.java]
 
         _binding = FragmentStressRecordBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
-        stressRecordViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        val chart = root.findViewById<LineChart>(R.id.chart1)
+
+
+        val values = ArrayList<Entry>()
+        values.add(Entry(2F, 3F))
+        values.add(Entry(4F, 10F))
+        val set1 = LineDataSet(values, "DataSet 1")
+        val dataSets = ArrayList<ILineDataSet>()
+        dataSets.add(set1)
+        val lineData = LineData(dataSets)
+        chart.data = lineData
         return root
     }
 
